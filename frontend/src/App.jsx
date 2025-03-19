cae7c79100b5bc0008695da14f51fda256e32657
maruf <marufbdn609@gmail.com>
2025-03-19 15:07:45 +0600

    home_about_component_route_create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './components/frontend/Home';
+import About from './components/frontend/About';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
       <button className='btn btn-danger'>submit</button>
     </>
   )
---
+++ frontend/src/components/frontend/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      he is monster
+    </div>
+  )
+}
+
+export default Home
+
