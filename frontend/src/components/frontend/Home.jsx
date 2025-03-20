a4366ff12f1651c390eba5b72e1cc36e7d62cdc5
maruf <marufbdn609@gmail.com>
2025-03-20 16:42:43 +0600

    footer_section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -64,7 +64,45 @@ const Home = () => {
         </section>
     </main>
     <footer>
-        
+        <div className='container'>
+            <div className="row yoo">
+                
+                <div className="col-md-3 mt-5">
+                    <h3 className='mb-3'>ZM construction</h3>
+                    <p>At ZM Construction, we offer expert project management services to guide your project from concept to completion. Our team ensures that every phase is executed smoothly, efficiently, and on budget.</p>
+                </div>
+                <div className="col-md-3 mt-5">
+                    <h3 className='mb-3'>Our Services</h3>
+                    <ul>
+                        <li><a href="">special construction</a></li>
+                        <li><a href="">special construction</a></li>
+                        <li><a href="">special construction</a></li>
+                        <li><a href="">special construction</a></li>
+                    </ul>
+                </div>
+                <div className="col-md-3 mt-5">
+                <h3 className='mb-3'>Quick Links</h3>
+                    <ul>
+                        <li><a href="">About us</a></li>
+                        <li><a href="">Services</a></li>
+                        <li><a href="">Blogs</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Contact us</a></li>
+                    </ul>
+                </div>
+                <div className="col-md-3 mt-5">
+                <h3 className='mb-3'>Contact Us</h3>
+                    <ul>
+                        <li><a href="">Phone:0178764544</a></li>
+                        <li><a href="">zm@gmail.com</a></li>
+                        <li><a href="">our address,special construction</a></li>
+                         
+                    </ul>
+                </div>
+               <hr />
+               <div className='text-center pt-2 pb-4'>Copyright © 2025 ZM Construction. All Rights Reserved</div>
+            </div>
+        </div>
     </footer>
     </>
   );
