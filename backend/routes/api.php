9c645eef735b209fd80fbc68d4f6eafded242acd
maruf <marufbdn609@gmail.com>
2025-03-23 16:19:52 +0600

    AuthenticationController_create_check

---
+++ backend/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('authenticate',[AuthenticationController::class,'authenticate']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
